19896709d0eb2134c6051c41c2c040313786bc4f
kbs5586 <52368748+kbs5586@users.noreply.github.com>
2019-10-07 19:58:37 +0900

    Drill 05
    2015180003 김병석
    Commit: 6회
    https://www.youtube.com/watch?v=7r3kZ7Ve6Wc&feature=youtu.be

--- Drill_05/Drill_05.py
+++ Drill_05/Drill_05.py
@@ -1,78 +1,72 @@
 from pico2d import *
 
-KPU_WIDTH, KPU_HEIGHT = 1280, 1024
-
 
 def handle_events():
     global running
-    global x, y
-    global handX, handy
-    global ClickX, ClickY
+    global v, u
+    global x2,y2
+    global Move
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEBUTTONDOWN and SDL_BUTTON_LEFT:
-            ClickX, ClickY = event.x, KPU_HEIGHT - 1 - event.y
-            MovePlayer((x, y), (ClickX, ClickY))
         elif event.type == SDL_MOUSEMOTION:
-            handX, handy = event.x, KPU_HEIGHT - 1 - event.y
+            v,u = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+                x2, y2 = event.x, KPU_HEIGHT - 1 - event.y
+                Move = True
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
 
-def MovePlayer(p1, p2):
-    global x, y
-    global ClickY, ClickY
-    global ResultX, ResultY
-    global IsStop
-    for i in range(0, 10000 + 1, 2):
-        t = i / 10000
-        x1 = ((1 - t) * p1[0] + t * p2[0]) * 0.001
-        y1 = ((1 - t) * p1[1] + t * p2[1]) * 0.001
-        if ClickX > x:
-            if ClickY > y:
-                x += x1
-                y += y1
-            elif ClickY < y:
-                x += x1
-                y -= y1
-        elif ClickX < x:
-            if ClickY > y:
-                x -= x1
-                y += y1
-            elif ClickY < y:
-                x -= x1
-                y -= y1
-        character.clip_draw(frame * 100, 100, 100, 100, x, y)
-    ResultX = x
-    ResultY = y
-
-
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
+
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
-hand = load_image('hand_arrow.png');
+Hand = load_image('hand_arrow.png')
 
-handX, handy = KPU_WIDTH // 2, KPU_HEIGHT // 2
-ResultX, ResultY = KPU_WIDTH // 2, KPU_HEIGHT // 2
-
-IsStop=False
-ClickX, ClickY = 0, 0
-Count = 0
 running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+
+x2, y2 = KPU_WIDTH // 2, KPU_HEIGHT // 2 #mouse 위치
+x1, y1 = KPU_WIDTH // 2, KPU_HEIGHT // 2 #현재위치
+u,v = 0,0
+Move = False
 frame = 0
+
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100, 100, 100, ResultX, ResultY)
-    hand.draw(handX + 22, handy - 20);
-    update_canvas()
-    frame = (frame + 1) % 8
+    Hand.draw(v, u)
 
+    if Move:
+        for i in range(0, 100 + 1, 2):
+            clear_canvas()
+            kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+            Hand.draw(v, u)
+
+            t = i / 100
+            x = (1 - t) * x1 + t * x2
+            y = (1 - t) * y1 + t * y2
+
+            if x2 > x1:
+                character.clip_draw(frame * 100, 100, 100, 100, x, y)
+            elif x2 < x1:
+                character.clip_draw(frame * 100, 0, 100, 100, x, y)
+            update_canvas()
+            frame = (frame + 1) % 8
+            delay(0.05)
+            handle_events()
+            if x == x2 and y == y2:
+                x1 = x2
+                y1 = y2
+                i = 0
+                Move = False
+    else:
+        character.clip_draw(frame * 100, 0, 100, 100, x1, y1)
+    update_canvas()
     handle_events()
 
 close_canvas()
