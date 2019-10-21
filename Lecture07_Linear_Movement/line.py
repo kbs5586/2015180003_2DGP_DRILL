f44b010c81eef27bb4b658bf6b7db90c5049062b
kbs5586 <dfd>
2019-10-21 17:28:41 +0900

    Init

--- Lecture07_Linear_Movement/line.py
+++ Lecture07_Linear_Movement/line.py
@@ -42,7 +42,7 @@ def draw_big_point(p):
     turtle.goto(p)
     turtle.color(0.8, 0.9, 0)
     turtle.dot(15)
-    turtle.write('     ' + str(p))
+    turtle.write('     '+str(p))
 
 
 def draw_point(p):
@@ -51,42 +51,17 @@ def draw_point(p):
 
 
 def draw_line_basic(p1, p2):
-    draw_big_point(p1)
-    draw_big_point(p2)
-
-    x1, y1 = p1[0], p1[1]
-    x2, y2 = p2[0], p2[1]
-    a = (y2 - y1) / (x2 - x1)
-    b = y1 - x1 * a
-
-    for x in range(x1, x2 + 1, 10):
-        y = a * x + b
-        draw_point((x, y))
-
-    draw_point(p2)
+    # fill here
+    pass
 
 
 def draw_line(p1, p2):
-    draw_big_point(p1)
-    draw_big_point(p2)
-
-    for i in range(0, 100 + 1, 2):
-        t = i / 100
-        x = (1 - t) * p1[0] + t * p2[0]
-        y = (1 - t) * p1[1] + t * p2[1]
-        draw_point((x, y))
-
-    draw_point(p2)
+    # fill here
+    pass
 
 
 prepare_turtle_canvas()
 
-size = 6
-points = [(random.randint(-500, 500), random.randint(-350, 350)) for
-          i in range(size)]
-n = 1
-while True:
-    draw_line(points[n - 1], points[n])
-    n = (n + 1) % size
+# fill here
 
 turtle.done()
