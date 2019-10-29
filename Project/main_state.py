8846c8f6d0f36b498b519e0f9612b8d16a340f35
kbs5586 <52368748+kbs5586@users.noreply.github.com>
2019-10-29 19:52:03 +0900

    Update Project

--- Project/main_state.py
+++ Project/main_state.py
@@ -8,24 +8,33 @@ from pico2d import *
 
 import game_framework
 
-logo_time = 0.0
+Monster_BulletTime = 0.0
+MidBoss_BulletTime = 0.0
+Player_BulletTime = 0.0
 Monster_time = 0.0
 Total_time = 0.0
 
 name = "MainState"
 x, y = 0, 0
 Player = None
-Bullet_Lst = []
-Monster_Lst = []
 Back_Ground_Img = None
-
 Item = None
+MidBoss = None
+
+Bullet_Lst = []
+MonsterBullet_Lst = []
+MidBossBullet_Lst = []
+Monster_Lst = []
+MidBoss_Lst = []
+Player_Lst = []
 
 
 class CPlayer:
     def __init__(self):
         self.x, self.y = x, y
         self.Bullet_Stack = 0
+        self.ImgX, self.ImgY = 115, 110
+        self.Hp = 1
         if Select_state.CharNum == 1:
             self.image = load_image('Player0.png')
         elif Select_state.CharNum == 2:
@@ -35,19 +44,52 @@ class CPlayer:
 
     def Update(self):
         global x, y
-        global logo_time
+        global Player_BulletTime
         global Bullet_Lst
-        if logo_time > 0.1:
+        if Player_BulletTime > 0.05:
             Bullet = CBullet()
             Bullet_Lst.append(Bullet)
-            logo_time = 0
+            Player_BulletTime = 0
         delay(0.01)
-        logo_time += 0.01
+        Player_BulletTime += 0.01
         self.x, self.y = x, y
 
     def Draw(self):
         global Bullet_Lst
         self.image.clip_draw(0, 0, 155, 110, self.x, self.y)
+
+
+class CMiBossBullet:
+    image = None
+
+    def __init__(self, x, y, Cnt):
+        self.x, self.y = x, y - 30
+        self.ImgX, self.ImgY = 45, 45
+        self.Cnt = Cnt
+        if CMiBossBullet.image is None:
+            CMiBossBullet.image = load_image('BossBullet0.png')
+
+    def Update(self):
+        if self.Cnt % 4 == 1:
+            self.x -= 2
+            self.y -= 5
+        elif self.Cnt % 4 == 2:
+            self.x -= 1
+            self.y -= 5
+        elif self.Cnt % 4 == 3:
+            self.x += 1
+            self.y -= 5
+        elif self.Cnt % 4 == 0:
+            self.x += 2
+            self.y -= 5
+        pass
+
+    def Draw(self):
+        CMiBossBullet.image.clip_draw(0, 0, self.ImgX, self.ImgY, self.x, self.y)
+        pass
+
+    def Collision(self, Unit):
+        pass
 
 
 class CBullet:
@@ -76,12 +118,45 @@ class CBullet:
 
     def Collision(self, Unit):
         for i in Unit:
-            if self.x - 34 < i.x + i.ImgX/2 \
-                    and self.y + 34 > i.y - i.ImgY/2 \
-                    and self.x + 34 > i.x - i.ImgX/2 \
-                    and self.y - 34 < i.y + i.ImgY/2:
-                del Unit[Unit.index(i)]
+            if self.x - 34 < i.x + i.ImgX / 2 \
+                    and self.y + 34 > i.y - i.ImgY / 2 \
+                    and self.x + 34 > i.x - i.ImgX / 2 \
+                    and self.y - 34 < i.y + i.ImgY / 2:
+                i.Hp -= 1
+                if i.Hp <= 0:
+                    del Unit[Unit.index(i)]
                 return True
+
+
+class CMonster_Bullet:
+    image = None
+
+    def __init__(self, x, y):
+        self.x, self.y = x, y - 10
+        self.ImgX, self.ImgY = 25, 25
+        if CMonster_Bullet.image is None:
+            CMonster_Bullet.image = load_image('Circle0.png')
+        pass
+
+    def Update(self):
+        self.y -= 5
+        pass
+
+    def Draw(self):
+        CMonster_Bullet.image.clip_draw(0, 0, self.ImgX, self.ImgY, self.x, self.y)
+        pass
+
+    def Collision(self, Unit):
+        for i in Unit:
+            if self.x - 12.5 < i.x + i.ImgX / 2 \
+                    and self.y + 12.5 > i.y - i.ImgY / 2 \
+                    and self.x + 12.5 > i.x - i.ImgX / 2 \
+                    and self.y - 12.5 < i.y + i.ImgY / 2:
+                i.Hp -= 1
+                if i.Hp <= 0:
+                    del Unit[Unit.index(i)]
+                return True
+        pass
 
 
 class CItem:
@@ -102,16 +177,59 @@ class CItem:
         pass
 
 
+class CBossMonster:
+    def __init__(self, BossNum):
+        self.x, self.y = 400, 600
+        self.ImgX, self.ImgY = 300, 300
+        self.Hp = 100
+        self.BulletCnt = 0
+        if BossNum == 0:
+            self.image = load_image('Boss0.png')
+        elif BossNum == 1:
+            pass
+
+    def Update(self):
+        global MidBoss_BulletTime
+        global MidBossBullet_Lst
+        if self.y <= 450:
+            pass
+        else:
+            self.y -= 1
+
+        if MidBoss_BulletTime > 0.5:
+            for i in range(4):
+                self.BulletCnt += 1
+                MonsterBullet = CMiBossBullet(self.x, self.y, self.BulletCnt)
+                MidBossBullet_Lst.append(MonsterBullet)
+            MidBoss_BulletTime = 0.0
+        MidBoss_BulletTime += 0.01
+
+    def Draw(self):
+        self.image.clip_draw(0, 0, self.ImgX, self.ImgY, self.x, self.y)
+
+
 class CMonster:
+    global MonsterBullet_Lst
+
     def __init__(self, xPos=400):
         self.x = xPos * 100
         self.y = 600
         self.image = load_image('Monster0.png')
         self.ImgX = 116
         self.ImgY = 120
+        self.Hp = 1
+        self.random = 0
 
     def Update(self):
+        global Monster_BulletTime
         self.y -= 1
+        if Monster_BulletTime > 1.0:
+            MonsterBullet = CMonster_Bullet(self.x, self.y)
+            MonsterBullet_Lst.append(MonsterBullet)
+
+            Monster_BulletTime = 0.0
+        Monster_BulletTime += 0.01
+
         pass
 
     def Draw(self):
@@ -119,11 +237,16 @@ class CMonster:
 
 
 def enter():
-    global Player, Item
+    global Player
     global Back_Ground_Img
+    global Player_Lst
+    global MidBoss_Lst
     Back_Ground_Img = load_image('BackGround.png')
     Player = CPlayer()
-    Item = CItem()
+    Player_Lst.append(Player)
+
+    MidBoss = CBossMonster(0)
+    MidBoss_Lst.append(MidBoss)
     pass
 
 
@@ -155,7 +278,9 @@ def handle_events():
 
 def update():
     global Monster_time, Total_time
-    global Monster_Lst, Bullet_Lst
+    global Monster_Lst, Bullet_Lst, MidBoss_Lst, Player_Lst
+    global MidBoss
+    global MonsterBullet_Lst
 
     if Monster_time > 1.0:
         Monster_lineLst = []
@@ -169,36 +294,58 @@ def update():
 
     Monster_time += 0.01
     Total_time += 0.01
-    if Total_time >= 30:
-        i = 0
+    if Total_time >= 10:
+        Total_time = 0
 
     for j in Bullet_Lst:
         if j.Collision(Monster_Lst):
             del Bullet_Lst[Bullet_Lst.index(j)]
+    for i in Bullet_Lst:
+        if i.Collision(MidBoss_Lst):
+            del Bullet_Lst[Bullet_Lst.index(i)]
+
+    # for i in MonsterBullet_Lst:
+    # i.Collision(Player_Lst)
+
+    if len(Player_Lst) <= 0:
+        pass
 
 
 def draw():
     global Back_Ground_Img
-    global Player, Item
     global Monster_Lst
     global Bullet_Lst
+    global MidBoss_Lst
+    global Player_Lst
+    global MidBossBullet_Lst
     clear_canvas()
     Back_Ground_Img.draw(400, 300)
 
-    if Player is not None:
-        Player.Update()
-        Player.Draw()
-    if Monster_Lst is not None:
-        for i in Monster_Lst:
-            i.Update()
-            i.Draw()
-            if i.y <= 0:
-                del Monster_Lst[Monster_Lst.index(i)]
-    if Bullet_Lst is not None:
-        for i in Bullet_Lst:
-            i.Update()
-            i.Draw()
-            if i.y >= 600:
-                del Bullet_Lst[Bullet_Lst.index(i)]
+    for i in Player_Lst:
+        i.Update()
+        i.Draw()
 
+    for i in Monster_Lst:
+        i.Update()
+        i.Draw()
+        if i.y <= 0:
+            del Monster_Lst[Monster_Lst.index(i)]
+
+    for i in Bullet_Lst:
+        i.Update()
+        i.Draw()
+        if i.y >= 600:
+            del Bullet_Lst[Bullet_Lst.index(i)]
+
+    for i in MidBoss_Lst:
+        i.Update()
+        i.Draw()
+
+    for i in MonsterBullet_Lst:
+        i.Update()
+        i.Draw()
+
+    for i in MidBossBullet_Lst:
+        i.Update()
+        i.Draw()
     update_canvas()
