357ee0cc4a346891a1ac1d3355c332dd612190f8
kbs5586 <52368748+kbs5586@users.noreply.github.com>
2019-11-12 22:59:04 +0900

    Drill 10 완성
    2015180003 김병석
    commit 횟수: 1회
    https://www.youtube.com/watch?v=0xpwE7itWVA&feature=youtu.be

---
+++ Drill_10/main_state.py
@@ -0,0 +1,57 @@
+import random
+import json
+import os
+
+from pico2d import *
+import game_framework
+import game_world
+
+from boy import Boy
+from grass import Grass
+import Bird
+
+name = "MainState"
+
+def enter():
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+    game_world.add_object(Bird.Bird(),1)
+
+def exit():
+    game_world.clear()
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                game_framework.quit()
+
+
+def update():
+    for game_object in game_world.all_objects():
+        game_object.update()
+    # fill here
+    delay(0.01)
+
+def draw():
+    clear_canvas()
+    for game_object in game_world.all_objects():
+        game_object.draw()
+    update_canvas()
+
+
+
+
+
+
